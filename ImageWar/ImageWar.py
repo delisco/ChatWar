8c2d7081927ad95b833296156f4deddf94c9ed73
Yich <yich.lin@xuenn.com>
2018-10-24 14:33:49 +0800

    rename folder and file

---
+++ ImageWar/ImageWar.py
@@ -0,0 +1,12 @@
+class ImgWar():
+    def __init__(self):
+        return
+    def printtext(self):
+        print('hello world')
+
+
+if __name__ == '__main__':
+    imgWar = ImgWar()
+    imgWar.printtext()
+
+
