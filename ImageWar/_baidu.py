b39da070201be4b67278715db6ea708677322d86
Yich <yich.lin@xuenn.com>
2018-10-25 14:38:52 +0800

    開始撰寫百度限制圖片筆數的邏輯

--- ImageWar/_baidu.py
+++ ImageWar/_baidu.py
@@ -26,7 +26,7 @@ class BaiduCrawler:
 
     # 获取图片url内容等
     # t 下载图片时间间隔
-    def __init__(self, t=0.1):
+    def __init__(self, t=0.05):
         self.time_sleep = t
 
     # 获取后缀名
@@ -45,45 +45,12 @@ class BaiduCrawler:
         else:
             return par.netloc
 
-        # 保存图片
-    # def save_image(self, rsp_data, word):
-    #     if not os.path.exists("./" + word):
-    #         os.mkdir("./" + word)
-    #     # 判断名字是否重复，获取图片长度
-    #     self.__counter = len(os.listdir('./' + word)) + 1
-    #     for image_info in rsp_data['imgs']:
-
-    #         try:
-    #             time.sleep(self.time_sleep)
-    #             suffix = self.get_suffix(image_info['objURL'])
-    #             # 指定UA和referrer，减少403
-    #             refer = self.get_referrer(image_info['objURL'])
-    #             opener = urllib.request.build_opener()
-    #             opener.addheaders = [
-    #                 ('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0'),
-    #                 ('Referer', refer)
-    #             ]
-    #             urllib.request.install_opener(opener)
-    #             # 保存图片
-    #             urllib.request.urlretrieve(image_info['objURL'], './' + word + '/' + str(self.__counter) + str(suffix))
-    #         except urllib.error.HTTPError as urllib_err:
-    #             print(urllib_err)
-    #             continue
-    #         except Exception as err:
-    #             time.sleep(1)
-    #             print(err)
-    #             print("产生未知错误，放弃保存")
-    #             continue
-    #         else:
-    #             print("小黄图+1,已有" + str(self.__counter) + "张小黄图")
-    #             self.__counter += 1
-    #     return
-
     # 开始获取
     def get_images(self, word):
         search = urllib.parse.quote(word)
         # pn int 图片数
         pn = self.__start_amount
+        result = []
         while pn < self.__amount:
 
             url = 'http://image.baidu.com/search/avatarjson?tn=resultjsonavatarnew&ie=utf-8&word=' + search + '&cg=girl&pn=' + str(
@@ -107,15 +74,15 @@ class BaiduCrawler:
                 # 解析json
                 rsp_data = json.loads(rsp)
                 # 取得第一張圖的網址
-                print(rsp_data['imgs'][0]['objURL'])
-                # self.save_image(rsp_data, word)
+                for image in rsp_data['imgs']:
+                    result.append(image['objURL'])
                 # 读取下一页
                 print("下载下一页")
                 pn += 60
             finally:
                 page.close()
         print("爬取任务结束")
-        return
+        return result
 
     def start(self, word, spider_page_num=1, start_page=1):
         """
@@ -127,12 +94,12 @@ class BaiduCrawler:
         """
         self.__start_amount = (start_page - 1) * 60
         self.__amount = spider_page_num * 60 + self.__start_amount
-        self.get_images(word)
+        result = self.get_images(word)
+        return result
 
 
 if __name__ == '__main__':
-    BaiduCrawler = BaiduCrawler(0.05)  # 抓取延迟为 0.05
+    BaiduCrawler = BaiduCrawler()
+    result = BaiduCrawler.start('cat', 1, 1) 
+    print(result)
 
-    # crawler.start('美女', 10, 2)  # 抓取关键词为 “美女”，总数为 1 页（即总共 1*60=60 张），开始页码为 2
-    BaiduCrawler.start('二次元 美女', 1, 1)  # 抓取关键词为 “二次元 美女”，总数为 10 页（即总共 10*60=600 张），起始抓取的页码为 1
-    # crawler.start('帅哥', 5)  # 抓取关键词为 “帅哥”，总数为 5 页（即总共 5*60=300 张）
