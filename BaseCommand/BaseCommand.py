cb8f3c5e47d6c09d876f523b62b19d742ac306e6
E5-572\User <gary.hong@conqtech.com>
2018-11-11 17:59:31 +0800

    no message

--- BaseCommand/BaseCommand.py
+++ BaseCommand/BaseCommand.py
@@ -4,7 +4,7 @@ import requests
 def BaseCommand(word):
 
     if word == 'help':
-        print("請自己想辦法")
+        print("現有功能：\n1.google搜尋，請試著使用 ./google \"關鍵字\" ，將顯示10筆搜尋結果。如想了解詳細設定參數請輸入 --man google 。\n2.stackoverflow搜尋，請試著使用 ./stackoverflow \"關鍵字\" ，將顯示10筆搜尋結果。\n3.圖戰功能，請試著使用 ./imagewar \"關鍵字\" ，將顯示最相關圖片。 \n4.熱門網站導址，請試著使用 ./apple ，將顯示最相似且熱門的導址。")
 
 keyword = input()
 BaseCommand(keyword)
