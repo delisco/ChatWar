8d7ec1605704b45ff3e2acb22457489424fbe220
Jason <empire5132@hotmail.com>
2019-02-24 01:24:29 +0800

    reply search result

--- route.py
+++ route.py
@@ -8,22 +8,12 @@ import hmac
 
 CHANNEL_ACCESS_TOKEN = 'Myk71sup6Hpu/KZ/9bAgUCiE0u//EolU3I6Exwx7JvlsJN4SRu3YCEASV16nGAwqEic+PGc+uutjC+uLYEZQ04ERYwbmROkvUFcrMUICu6ndEP1EioNzdQtbk/kRMR7PQeatI5aOg+ApNlKqqbRSrQdB04t89/1O/w1cDnyilFU='
 
-# class MessageResponse(object):
-#     messageType = ''
-#     messageText = ''
-
-#     def __init__(self, responseType = 'text', responseText = 'test'):
-#         self.messageType = responseType
-#         self.messageText = responseText
-
 @hug.post('/googleSearch')
 def googleSearchFunction(body):
-    print(body)
     client = linebot.LineBotApi(CHANNEL_ACCESS_TOKEN)
     reply_token = body['events'][0]['replyToken']
-    message = linebot.models.TextMessage(text = body['events'][0]['message']['text'])
-    print(reply_token)
-    print(body['events'][0]['message']['text'])
+    message = linebot.models.TextMessage(text = googleSearch.googleSearch(body['events'][0]['message']['text']))
+    # message = linebot.models.TextMessage(text = body['events'][0]['message']['text'])
     client.reply_message(reply_token, message)
 
 if __name__ == '__main__':
