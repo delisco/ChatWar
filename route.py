0590cd48ecac2a070fb759e78731b7b9fd199106
Jason <empire5132@hotmail.com>
2019-02-24 01:13:06 +0800

    array message

--- route.py
+++ route.py
@@ -21,10 +21,10 @@ def googleSearchFunction(body):
     print(body)
     client = linebot.LineBotApi(CHANNEL_ACCESS_TOKEN)
     reply_token = body['events'][0]['replyToken']
-    message = MessageResponse('text', [body['events'][0]['message']['text']])
+    message = MessageResponse('text', body['events'][0]['message']['text'])
     print(reply_token)
     print(body['events'][0]['message']['text'])
-    client.reply_message(reply_token, message)
+    client.reply_message(reply_token, [message])
 
 if __name__ == '__main__':
     print('test')
