f7e236c94963f7f1efeb6af8fa0eee3cf7bd9fc2
Jason <empire5132@hotmail.com>
2019-02-23 23:52:27 +0800

    to verify line webhook

--- route.py
+++ route.py
@@ -1,11 +1,20 @@
 import hug
 import Search.googleSearch as googleSearch
 import json
+import linebot
+
+class googleSearchResponse:
+    replyToken = ''
+    type = ''
+    timestamp = ''
+    source = None # type userId
+    message = None # id type text
 
 @hug.post('/googleSearch')
 def googleSearchFunction(body):
     return {
-        'message': googleSearch.googleSearch(body['keyword'])
+        # 'message': googleSearch.googleSearch(body['keyword'])
+        'message': 'test'
     }
 
 if __name__ == '__main__':
