50ad089fb63de69967a23e4cdc56fba5add8908b
Jason <empire5132@hotmail.com>
2019-02-24 16:11:12 +0800

    update file name

--- Config/Config.py
+++ Config/Config.py
@@ -8,7 +8,7 @@ class Config:
     def get(self, section, key):
         config = configparser.ConfigParser()
         file_root = os.path.abspath(__file__)
-        path = os.path.join(file_root, 'Config.conf')
+        path = os.path.join(file_root, 'Config.ini')
         config.read(path)
         print(config.sections())
         return config['chatWar']['CHANNEL_ACCESS_TOKEN']
