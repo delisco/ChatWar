60a9df22515fe182b5a5fe1279c37c03db61c4da
Jason <empire5132@hotmail.com>
2019-02-24 15:53:27 +0800

    use abspath

--- Config/Config.py
+++ Config/Config.py
@@ -1,15 +1,13 @@
 import configparser
 import os
 
-CONFIG_PATH = 'Config/Config.conf'
-
 class Config:
     def __init__(self):
         pass
 
     def get(self, section, key):
-        config = configparser.ConfigParser(CONFIG_PATH)
-        file_root = os.path.dirname(__file__)
+        config = configparser.ConfigParser()
+        file_root = os.path.abspath(__file__)
         path = os.path.join(file_root, 'Config.conf')
         config.read(path)
         return config.get(section, key)
