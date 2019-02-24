4c416101b04bdb719e6a20760abea0fb079d7ef7
Jason <empire5132@hotmail.com>
2019-02-24 15:43:20 +0800

    set config file path

--- Config/Config.py
+++ Config/Config.py
@@ -1,12 +1,14 @@
 import configparser
 import os
 
+CONFIG_PATH = 'Config/Config.conf'
+
 class Config:
     def __init__(self):
         pass
 
     def get(self, section, key):
-        config = configparser.ConfigParser()
+        config = configparser.ConfigParser(CONFIG_PATH)
         file_root = os.path.dirname(__file__)
         path = os.path.join(file_root, 'Config.conf')
         config.read(path)
