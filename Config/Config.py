24537c46c74abf9efbfb727b024e269e896ecdda
Jason <empire5132@hotmail.com>
2019-02-24 16:06:56 +0800

    debug

--- Config/Config.py
+++ Config/Config.py
@@ -10,4 +10,5 @@ class Config:
         file_root = os.path.abspath(__file__)
         path = os.path.join(file_root, 'Config.conf')
         config.read(path)
+        print(config.sections())
         return config['chatWar']['CHANNEL_ACCESS_TOKEN']
